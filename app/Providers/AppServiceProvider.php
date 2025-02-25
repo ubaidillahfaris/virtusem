ea7da93f7c2288e3c1f78a4793ea1f4af4442211
faris <ubaidillah.faris99@gmail.com>
2025-02-25 16:07:00 +0700

    install predis

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +23,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Rate Limiter Global untuk semua request di web.php
+    RateLimiter::for('global', function (Request $request) {
+        return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+    });
+
+    // Rate Limiter untuk API
+    RateLimiter::for('api', function (Request $request) {
+        return Limit::perMinute(100)->by(optional($request->user())->id ?: $request->ip());
+    });
+
+    RateLimiter::for('global', function (Request $request) {
+        return Limit::perMinute(60)
+            ->by(optional($request->user())->id ?: $request->ip())
+            ->response(function () {
+                return response()->json('Too Many Requests, Chill!!',429);
+            });
+    });
+    
+
         Vite::prefetch(concurrency: 3);
     }
 }
